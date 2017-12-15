b395a7443565fa95afa6ef585bad0b4a7deac096
saddam1997 <saddam.husen1997@gmail.com>
2017-12-15 19:24:39 +0530

    Bid and ask for all remain currencies

---
+++ api/models/BidSEK.js
@@ -0,0 +1,81 @@
+/**
+ * BidSEK.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+var moment = require('moment');
+module.exports = {
+
+  schema: true,
+  attributes: {
+    createTimeUTC: {
+      type: 'string',
+    },
+    askAmountBTC: {
+      type: 'float',
+      defaultsTo: 0.00,
+      required: true
+    },
+    askAmountSEK: {
+      type: 'float',
+      defaultsTo: 0.00,
+      required: true
+    },
+    totalaskAmountBTC: {
+      type: 'float',
+      defaultsTo: 0.00,
+      required: true
+    },
+    totalaskAmountSEK: {
+      type: 'float',
+      defaultsTo: 0.00,
+      required: true
+    },
+    askAmountLTC: {
+      type: 'float',
+      defaultsTo: 0.00,
+      required: true
+    },
+    totalaskAmountLTC: {
+      type: 'float',
+      defaultsTo: 0.00,
+      required: true
+    },
+    askAmountBCH: {
+      type: 'float',
+      defaultsTo: 0.00,
+      required: true
+    },
+    totalaskAmountBCH: {
+      type: 'float',
+      defaultsTo: 0.00,
+      required: true
+    },
+    askRate: {
+      type: 'float',
+      required: true,
+      defaultsTo: 0
+    },
+    status: {
+      type: 'integer'
+    },
+    statusName: {
+      type: 'string'
+    },
+    marketId: {
+      type: 'integer'
+    },
+    askownerSEK: {
+      model: 'user'
+    }
+  },
+  afterCreate: function(values, next) {
+    //values.createTimeUTC = moment.utc().format();
+    values.createTimeUTC = Date.parse(moment.utc().format()) / 1000;
+    AskSEK.update({
+      id: values.id
+    }, values, next);
+  }
+
+};

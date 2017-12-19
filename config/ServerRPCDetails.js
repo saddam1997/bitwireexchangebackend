99d1c37e2efa6ac4c39ad7107e1bd8ef2e70d42f
saddam1997 <saddam.husen1997@gmail.com>
2017-12-19 16:04:31 +0530

    add all bitcoinclient details

--- config/ServerRPCDetails.js
+++ config/ServerRPCDetails.js
@@ -24,4 +24,110 @@ module.exports.company = {
   clientINRuser: 'test',
   clientINRpass: 'test123',
 
+  //USD Server details
+  clientUSDhost: '162.213.252.66',
+  clientUSDport: 18336,
+  clientUSDuser: 'test',
+  clientUSDpass: 'test123',
+
+
+  //EUR Server details
+  clientEURhost: '162.213.252.66',
+  clientEURport: 18336,
+  clientEURuser: 'test',
+  clientEURpass: 'test123',
+
+
+  //GBP Server details
+  clientGBPhost: '162.213.252.66',
+  clientGBPport: 18336,
+  clientGBPuser: 'test',
+  clientGBPpass: 'test123',
+
+
+  //BRL Server details
+  clientBRLhost: '162.213.252.66',
+  clientBRLport: 18336,
+  clientBRLuser: 'test',
+  clientBRLpass: 'test123',
+
+
+  //PLN Server details
+  clientPLNhost: '162.213.252.66',
+  clientPLNport: 18336,
+  clientPLNuser: 'test',
+  clientPLNpass: 'test123',
+
+
+  //CAD Server details
+  clientCADhost: '162.213.252.66',
+  clientCADport: 18336,
+  clientCADuser: 'test',
+  clientCADpass: 'test123',
+
+
+  //TRY Server details
+  clientTRYhost: '162.213.252.66',
+  clientTRYport: 18336,
+  clientTRYuser: 'test',
+  clientTRYpass: 'test123',
+
+
+  //RUB Server details
+  clientRUBhost: '162.213.252.66',
+  clientRUBport: 18336,
+  clientRUBuser: 'test',
+  clientRUBpass: 'test123',
+
+
+  //MXN Server details
+  clientMXNhost: '162.213.252.66',
+  clientMXNport: 18336,
+  clientMXNuser: 'test',
+  clientMXNpass: 'test123',
+
+
+  //CZK Server details
+  clientCZKhost: '162.213.252.66',
+  clientCZKport: 18336,
+  clientCZKuser: 'test',
+  clientCZKpass: 'test123',
+
+
+  //ILS Server details
+  clientILShost: '162.213.252.66',
+  clientILSport: 18336,
+  clientILSuser: 'test',
+  clientILSpass: 'test123',
+
+
+  //NZD Server details
+  clientNZDhost: '162.213.252.66',
+  clientNZDport: 18336,
+  clientNZDuser: 'test',
+  clientNZDpass: 'test123',
+
+
+  //JPY Server details
+  clientJPYhost: '162.213.252.66',
+  clientJPYport: 18336,
+  clientJPYuser: 'test',
+  clientJPYpass: 'test123',
+
+
+  //SEK Server details
+  clientSEKhost: '162.213.252.66',
+  clientSEKport: 18336,
+  clientSEKuser: 'test',
+  clientSEKpass: 'test123',
+
+
+  //AUD Server details
+  clientAUDhost: '162.213.252.66',
+  clientAUDport: 18336,
+  clientAUDuser: 'test',
+  clientAUDpass: 'test123',
+
+
+
 }
